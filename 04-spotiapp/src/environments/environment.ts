2c59fac3a16c9004c2b894f6573b90e769b9489a
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-06-15 18:42:55 -0500

    Removed environment file

--- 04-spotiapp/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  spoti_api_url: 'https://api.spotify.com/v1',
-  spoti_preview_track_url: 'https://open.spotify.com/embed/track/',
-  spoti_token_url: 'https://accounts.spotify.com/api/token',
-  spoti_client_id: 'aa5615faaf8749edb25d73837c4888a8',
-  spoti_client_secret: 'dfecab14e4964ea99a1edddbc377d192',
-};
