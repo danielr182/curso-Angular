aeb266dcbe0b5edab89e30f67bcb1e88a4bdefd8
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-06-15 18:38:59 -0500

    Added missed types

--- 04-spotiapp/src/app/pipes/noimage.pipe.ts
+++ 04-spotiapp/src/app/pipes/noimage.pipe.ts
@@ -1,17 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Image } from '../shared/interfaces/spoti-common';
 
 @Pipe({
   name: 'noimage',
-  standalone: true
+  standalone: true,
 })
 export class NoimagePipe implements PipeTransform {
-
-  transform(images: any[]): string {
+  transform(images: Image[]): string {
     if (images && images.length > 0) {
       return images[0].url;
     } else {
       return 'assets/img/noimage.png';
     }
   }
-
 }
